a1657f3888521b565058f1ece54d3be5f7fb9973
adeilyas <adeilyas@gmail.com>
2024-07-30 01:40:34 +0700

    add : daftar pengguna dan perhitungan BMI

--- utama.cpp
+++ utama.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <conio.h>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 struct imb{          
   int tinggiBadan;
   int beratBadan;
-  int jk; 
+  char jk; 
   int usia;
   string nama;
+  float bmi;
 }; 
 imb pengguna[10];
-int posisi=0;
+int posisi=-1;
 string kategImt[5];
 
 
@@ -44,7 +46,20 @@ void refBMI(){
   getch();
 }
 
+int cekBmi(float data){
 
+if(data<17.0)
+return 0;
+else if(data<=18.4)
+return 1;
+else if(data <= 25.0)
+  return 2;
+else if(data<=27.0)
+  return 3;
+ else
+return 4;
+
+}
 void aDev(){
   system("cls");
   cout<<"Our Team\n\n";
@@ -60,6 +75,84 @@ void awal(){
   kategImt[3]="25,1-27,0 termasuk dalam kategori gemuk tingkat ringan";
   kategImt[4]="Di atas 27 termasuk dalam kategori gemuk tingkat berat";
 }
+
+char hurufBesar(char masuk){
+  if(masuk=='l')
+  return 'L';
+  else if(masuk=='p')
+  return 'P';
+  else if(masuk=='P' || masuk=='L')
+  return masuk;
+  else
+  return '-';
+}
+float getBmi(float tinggi,float berat){
+float mTinggi=tinggi/100;
+float hasil=berat/(mTinggi*mTinggi);
+return hasil;
+}
+void hitung (){
+system("cls");
+posisi++;
+char jk;
+int tinggi;
+cout <<"Hitung Indeks Massa Tubuh\n\n";
+cout <<"Berat Badan    [kg]: ";
+cin >> pengguna[posisi].beratBadan;
+cout <<"Tinggi Badan   [cm]: ";
+cin >> pengguna[posisi].tinggiBadan;
+cout <<"Jenis Kelamin (L/P): ";
+cin >> jk;
+pengguna[posisi].jk=hurufBesar(jk);
+cout <<"Usia               : ";
+cin >> pengguna[posisi].usia;
+cout <<"Nama               : ";
+cin >> pengguna[posisi].nama;
+
+pengguna[posisi].bmi=getBmi(pengguna[posisi].tinggiBadan,pengguna[posisi].beratBadan);
+
+cout <<"Hasil BMI          : "<<pengguna[posisi].bmi;
+cout <<"\n"<<kategImt[cekBmi(pengguna[posisi].bmi)];
+
+getch();
+}
+
+void sampelData(){
+  pengguna[posisi].beratBadan=67;
+  pengguna[posisi].tinggiBadan=158;
+  pengguna[posisi].usia=30;
+  pengguna[posisi].jk='L';
+  pengguna[posisi].nama="Pengguna Aplikasi Laki-laki";
+  pengguna[posisi].bmi=0.0;
+  posisi++;
+  pengguna[posisi].beratBadan=58;
+  pengguna[posisi].tinggiBadan=151;
+  pengguna[posisi].usia=28;
+  pengguna[posisi].jk='P';
+  pengguna[posisi].nama="Pengguna Aplikasi Perempuan";
+  pengguna[posisi].bmi=0.0;
+  posisi++;
+
+
+}
+void listPengguna(){
+int count;
+system("cls");
+if(posisi>=0){
+cout<<"Daftar Pengguna :\n";
+for (int i = 0; i <= posisi; i++)
+{
+  cout<<i+1<<". "<<pengguna[i].jk<<" # "<<pengguna[i].bmi<<" # "<<kategImt[cekBmi(pengguna[i].bmi)]<<"\n";
+}
+
+
+}else{
+  cout<<"kosong";
+}
+
+
+}
+
 int main() {
 awal();
 char pl;
@@ -71,10 +164,12 @@ do
   {
    case '1':
     /* code */
-    mPertama("pertama");
+    hitung();
     break;
    case '2':
-    mPertama("ke- dua");
+    //mPertama("ke- dua");
+    listPengguna();
+    getch();
     /* code */ 
     break;  
    case '3':
